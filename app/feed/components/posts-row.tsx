b5451f11f84cd26765d8074a2e6a2072b767a8e3
Orishmueli <43569659+OriShmueli@users.noreply.github.com>
2023-10-10 23:04:11 +0300

    added post report grid

--- app/feed/components/posts-row.tsx
+++ app/feed/components/posts-row.tsx
@@ -1,12 +1,9 @@
 // TODO: Add Data Table view and posts view
 "use client"
-
 import useEmblaCarousel from "embla-carousel-react";
 import { useState, useEffect, useCallback } from "react";
 
-export default function PostBoard({posts} : {posts : any[]}) {
-
- 
+export default function PostRow({posts} : {posts : any[]}) {
 
     return (
         <div className="flex flex-row w-2/3 gap-2">           
--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,19 +1,91 @@
+"use client"
 import Image from 'next/image'
 import { Button } from '@/components/ui/button';
 import Post from '@/components/post';
-import PostBoard from './components/posts-board';
+import PostRow from './components/posts-row';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [posts, setPosts] = useState<any[]>([
+  
+    [
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'}
+    ],
+    [
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'}
+    ],
+    [
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'}
+    ],
+    [
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'}
+    ],
+    [
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'},
+      {link: 'https://vt.tiktok.com/ZSNYeNDvt/', isSupport: true, platform: 'tiktok'}
+    ],
+
+  ])
   
   return (
-    <div className="w-full xl:w-[80rem] min-h-full flex flex-col items-center text-foreground gap-6">
-      {/* // TODO: Add Data Table view and posts view */}
-      <PostBoard posts={[
-        <Post key={1} link=''  isSupport platform='' />,
-        <Post key={1} link=''  isSupport platform='' />,
-        <Post key={1} link=''  isSupport platform='' />,
-        <Post key={1} link=''  isSupport platform='' />,
-      ]}  />
+    
+    <div className="w-full xl:w-[80rem] min-h-full flex flex-col items-center text-foreground gap-6 pt-2">
+     <Tabs defaultValue='postsReport' className='flex flex-col items-center justify-center gap-4'>
+            <TabsList>
+                <TabsTrigger value='postsReport' onClick={ () => alert("loading report...") }>Post Reports</TabsTrigger>
+                <TabsTrigger value='postsSupport' onClick={ () => alert("loading support...") }>Post Support</TabsTrigger>
+            </TabsList>     
+            <TabsContent value='postsReport' className='flex flex-col gap-4'>
+
+              {
+               
+                posts.map((postRow, index) => (
+                  <PostRow posts={[
+                    <Post key={1} link={postRow[0].link} isSupport={postRow[0].isSupport} platform={postRow[0].platform} />,
+                    <Post key={1} link={postRow[1].link} isSupport={postRow[1].isSupport} platform={postRow[1].platform} />,
+                    <Post key={1} link={postRow[2].link} isSupport={postRow[2].isSupport} platform={postRow[2].platform} />,
+                    <Post key={1} link={postRow[3].link} isSupport={postRow[3].isSupport} platform={postRow[3].platform} />,
+                  ]} />
+                ))
+
+                
+              }  
+            
+            </TabsContent>
+            <TabsContent value='postsSupport' className='flex flex-col gap-4'>
+                            
+              {
+                
+                posts.map((postRow, index) => (
+                  <PostRow posts={[
+                    <Post key={1} link={postRow[0].link} isSupport={postRow[0].isSupport} platform={postRow[0].platform} />,
+                    <Post key={1} link={postRow[1].link} isSupport={postRow[1].isSupport} platform={postRow[1].platform} />,
+                    <Post key={1} link={postRow[2].link} isSupport={postRow[2].isSupport} platform={postRow[2].platform} />,
+                    <Post key={1} link={postRow[3].link} isSupport={postRow[3].isSupport} platform={postRow[3].platform} />,
+                  ]} />
+                ))
+
+                
+              }  
+            </TabsContent>
+        </Tabs>    
+      
     </div>
   )
 }
