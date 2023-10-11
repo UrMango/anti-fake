47bbc4489d4f56e31129b7f54b5940126bda54a0
NoamNaim <82314466+NoamMeshoomar@users.noreply.github.com>
2023-10-11 20:51:32 +0300

    API: Get Links endpoint with Pagination

--- app/utils/supabaseConnection.ts
+++ app/utils/supabaseConnection.ts
@@ -1,6 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { createClient } from '@supabase/supabase-js'
-import { Database } from './supabase'
+import { createClient } from '@supabase/supabase-js';
+import { Database } from './supabase';
 
 
 export const supabaseConnection = createClient<Database>(
@@ -8,10 +7,10 @@ export const supabaseConnection = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
 )
  
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const allOnlineUsers = await supabaseConnection.from('users').select('*').eq('status', 'ONLINE')
-  res.status(200).json(allOnlineUsers)
-}
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   const allOnlineUsers = await supabaseConnection.from('users').select('*').eq('status', 'ONLINE')
+//   res.status(200).json(allOnlineUsers);
+// }
 
 // npx supabase gen types typescript --project-id "wpaztqhaznrhfwlbnfvn" --schema public > types/supabase.ts
 
