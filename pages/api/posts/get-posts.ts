22e488a0a8baf35aefe650c6df6be8ed03ad586b
NoamNaim <82314466+NoamMeshoomar@users.noreply.github.com>
2023-10-11 20:51:32 +0300

    API: Get Links endpoint with Pagination

--- pages/api/posts/get-posts.ts
+++ pages/api/posts/get-posts.ts
@@ -1,6 +1,23 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { supabaseConnection } from '@/app/utils/supabaseConnection';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  
-}
+    const {limit = 20, skip = 0} = req.query;
 
+    try {
+        if(typeof +limit !== "number" || typeof +skip !== "number") return;
+
+        const posts = (await supabaseConnection
+            .from("links")
+            .select("id, link, action")
+            .limit(+limit)
+            .range(+skip, +skip + +limit)
+            .eq("active", true)
+            .neq("action", '')).data;
+
+        res.status(200).json(posts);
+    } catch (err) {
+        console.error(err);
+        res.status(404).json({error: "Links not found!"});
+    }
+}
