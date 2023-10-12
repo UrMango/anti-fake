37d35fb9467ef195e0cb7c06c5e5ca0a0c5f78fe
NoamNaim <82314466+NoamMeshoomar@users.noreply.github.com>
2023-10-12 09:23:49 +0300

    Update get-posts.ts

--- pages/api/posts/get-posts.ts
+++ pages/api/posts/get-posts.ts
@@ -1,26 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabaseConnection } from '@/app/utils/supabaseConnection';
 
+const postTypes = ["repost", "report"];
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const {limit = 20, skip = 0, type = 1} = req.query;
 
     try {
-        if(typeof +limit !== "number" || typeof +skip !== "number") return;
-        let post = supabaseConnection
-        .from("links")
-        .select("id, link, action")
-        .limit(+limit - 1)
-        .range(2 + +skip,2 + +skip + +limit-1)
-        .eq("active", true)
-        .neq("action", '');
-        if(+type == 1) post = post.eq("action", 'repost');
-        if(+type == 2) post = post.eq("action", 'report');
+        if(
+            typeof +limit !== "number" || 
+            typeof +skip !== "number" || 
+            typeof +type !== "number"
+        ) throw "Invalid request";
 
-        const posts = (await post).data;
-        console.log(posts);
-        res.status(200).json(posts);
-    } catch (err) {
-        console.error(err);
-        res.status(404).json({error: "Links not found!"});
+        const posts = await supabaseConnection
+            .from("links")
+            .select("id, link, action", {count: "exact"})
+            .limit(+limit - 1)
+            .range(2 + +skip, 2 + +skip + +limit - 1)
+            .neq("action", '')
+            .eq("active", true)
+            .eq("action", postTypes[+type]);
+
+        res.status(200).json({posts: posts.data || [], count: posts.count || 0});
+    } catch(error) {
+        res.status(404).json({error});
     }
 }
